3da00cd3bf6737819e215bd255c60b5144826291
LazarRajicic02 <lazarrajicic02@gmail.com>
2024-02-26 15:55:54 +0100

    Update

--- src/components/CourseGoalList.tsx
+++ src/components/CourseGoalList.tsx
@@ -1,20 +1,36 @@
+import { type ReactNode } from "react";
 import { CourseGoal as CGoal } from "../App";
 import CourseGoal from "./CourseGoal";
+import InfoBox from "./Info";
 
 interface CourseGoalProps {
   goals: CGoal[];
   onDelete: (id: number) => void;
 }
 export default function CourseGoalList({ goals, onDelete }: CourseGoalProps) {
+  if (goals.length === 0) {
+    return <InfoBox mode="hint">You have no course goals yet</InfoBox>;
+  }
+  let warningBox: ReactNode;
+  if (goals.length >= 4) {
+    warningBox = (
+      <InfoBox mode="warning" severity="high">
+        You're collecting a lot of goals.Don't put too much on your plate!
+      </InfoBox>
+    );
+  }
   return (
-    <ul>
-      {goals.map((item) => (
-        <li key={item.id}>
-          <CourseGoal id={item.id} title={item.title} onDelete={onDelete}>
-            <p>{item.description}</p>
-          </CourseGoal>
-        </li>
-      ))}
-    </ul>
+    <>
+      {warningBox}
+      <ul>
+        {goals.map((item) => (
+          <li key={item.id}>
+            <CourseGoal id={item.id} title={item.title} onDelete={onDelete}>
+              <p>{item.description}</p>
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
--- src/components/NewGoal.tsx
+++ src/components/NewGoal.tsx
@@ -1,33 +1,46 @@
-import { useRef, type FormEvent } from "react";
+import { useRef, useState, type FormEvent, ReactNode } from "react";
+import InfoBox from "./Info";
 
 interface NewGoalProps {
   onAddGoal: (goal: string, summary: string) => void;
 }
 
 export function NewGoal({ onAddGoal }: NewGoalProps) {
+  const [error, setError] = useState<ReactNode>(null);
   const goalRef = useRef<HTMLInputElement>(null);
   const summaryRef = useRef<HTMLInputElement>(null);
   function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
     const enteredGoal = goalRef.current!.value;
     const summary = summaryRef.current!.value;
+    if (enteredGoal === "") {
+      return setError(
+        <InfoBox mode="warning" severity="high">
+          Insert values for your goal and summary inputs.
+        </InfoBox>
+      );
+    }
+    setError(null);
+
     event.currentTarget.reset();
     onAddGoal(enteredGoal, summary);
   }
-
   return (
-    <form onSubmit={handleSubmit}>
-      <p>
-        <label htmlFor="goal">Your goal</label>
-        <input id="goal" type="text" ref={goalRef} />
-      </p>
-      <p>
-        <label htmlFor="summary">Short summary</label>
-        <input id="summary" type="text" name="summary" ref={summaryRef} />
-      </p>
-      <p>
-        <button>Add goal</button>
-      </p>
-    </form>
+    <>
+      <form onSubmit={handleSubmit}>
+        <p>
+          <label htmlFor="goal">Your goal</label>
+          <input id="goal" type="text" ref={goalRef} />
+        </p>
+        <p>
+          <label htmlFor="summary">Short summary</label>
+          <input id="summary" type="text" name="summary" ref={summaryRef} />
+        </p>
+        <p>
+          <button>Add goal</button>
+        </p>
+      </form>
+      {error}
+    </>
   );
 }
